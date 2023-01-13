9ee2c283a7562cc42817ea0a3933c6e8e6f69c65
wibmw <ilaouer@outlook.com>
2023-01-13 14:20:34 +0100

    Export IForm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import CustomInput from './components/CustomInput'
 import CustomSelect from './components/CustomSelect'
+import IForm from './components/InputWrapper'
 
-export { CustomInput, CustomSelect }
+export { CustomInput, CustomSelect, IForm }
